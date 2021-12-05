e2272bffd61bcc5ac2e760264235a1d125922be4
Jordan van Beijnhem <jordanvanbeijnhem@gmail.com>
2021-12-05 07:18:49 +0100

    Add day 5, puzzle 1

--- src/main/java/nl/jordanvanbeijnhem/Main.java
+++ src/main/java/nl/jordanvanbeijnhem/Main.java
@@ -25,7 +25,8 @@ public class Main {
         puzzleManager.addPuzzle(new Day03_1("03_1", new File(classLoader.getResource("input/03_1.txt").getFile())));
         puzzleManager.addPuzzle(new Day04_0("04_0", new File(classLoader.getResource("input/04_0.txt").getFile())));
         puzzleManager.addPuzzle(new Day04_1("04_1", new File(classLoader.getResource("input/04_1.txt").getFile())));
+        puzzleManager.addPuzzle(new Day05_0("05_0", new File(classLoader.getResource("input/05_0.txt").getFile())));
 
-        puzzleManager.runPuzzle("04_1");
+        puzzleManager.runPuzzle("05_0");
     }
 }
